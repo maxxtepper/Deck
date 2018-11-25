47dc4175eded0fa7362e955359949935beb4e658
maxxtepper <maxxtepper@gmail.com>
2018-11-24 18:49:01 -0800

    The Deck. Adding new git: The Deck Maker

--- Card.h
+++ Card.h
@@ -1,12 +1,21 @@
 #ifndef Card.H
 #define Card.H
 
+
 class Card {
 	public:
-		Card();
+		Card(int suit, int value);
 		~Card();
+		int enum{CLUB,DIAMOND,HEART,SPADE};
+		int enum{ACE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,JOKER};
+
+		int getSuite();
+		int getValue();
 
 	private:
+		//	Together, the suite and the value create the full unique id of each card
+		int _suite;
+		int _value;	// same as id
 };
 
 #endif
